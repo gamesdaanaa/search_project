3f29fe887689e497f8c248634fcaa0ead206d6c5
佐々木ダアナア <sasakidaanaa@sasakidaanaanoMacBook-Air.local>
2024-11-06 17:00:41 +0900

    初回コミット

---
+++ search_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+ 
+class Category(models.Model): 
+    name = models.CharField(max_length=255) 
+ 
+    def __str__(self): 
+        return self.name 
+ 
+class Product(models.Model): 
+    id = models.BigAutoField(primary_key=True) 
+    name = models.CharField(max_length=255) 
+    description = models.TextField() 
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)  # 1はカテゴリID 
+    image = models.ImageField(upload_to='images/', verbose_name="画像ファイル")
+    def __str__(self): 
+        return self.name
